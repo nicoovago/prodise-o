329a6ff0be99a1968d8a5aa151f30c4b86937e71
nicoovago <nicolasjvago@gmail.com>
2026-06-10 16:51:53 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,9 @@
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mecszydokfgmjgfxdsoy.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lY3N6eWRva2ZnbWpnZnhkc295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMTA4MDcsImV4cCI6MjA5NjY4NjgwN30.NRmDS-3ydIZmDj6SM7Pk08l4fi5uFWRo5VsMfrPDWt0'
-
 export const supabase = createClientComponentClient()
 
-export const supabaseAdmin = createClient(supabaseUrl, supabaseAnonKey)
+export const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+)
